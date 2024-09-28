f72f2feb0646682b509e2f306f0a2721e1f2d897
wahabsukoraharjo <wahab.sukoraharjo@gmail.com>
2024-09-28 11:46:36 +0700

    update profile2

---
+++ app/Views/v_profile3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ini profile
+    
+    <a href="/profile">Kembali ke home</a>
+</body>
+</html>
